746d6fd8e1ad1e0002d3aca336f15b595e49aba7
FYR666 <31131580+FYR666@users.noreply.github.com>
2018-09-20 19:53:11 +0800

    Practice

---
+++ practice22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y;
+            Console.WriteLine("请输入x的值：");
+            x = float.Parse(Console.ReadLine());
+            Console.WriteLine("请输入y的值：");
+            y = float.Parse(Console.ReadLine());
+            float max = x > y ? x : y;
+            Console.WriteLine("max="+max);
+            Console.ReadKey();
+        }
+    }
+}
